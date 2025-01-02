d039ea4b899453f5468c8bcf2881b0120b60420b
Mel <garrillofeeling@gmail.com>
2025-01-02 15:28:18 -0400

    Create number button loading function

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,22 @@
+let numbers;
+
 function loadData() {
-    
+    numbers = document.getElementById("numerical");
+    console.log("hi");
+    loadButtons();
+}
+
+function createNumberButton(number) {
+    const newNumber = document.createElement("button");
+    newNumber.textContent = number;
+    newNumber.value = number;
+    return newNumber;
+}
+
+function loadButtons() {
+    for (let i = 1; i < 10; i++)
+        numbers.appendChild(createNumberButton(i));
+    numbers.appendChild(createNumberButton(0))
 }
 
 function operate(operation, a, b) {
