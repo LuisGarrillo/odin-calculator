f9e5d757b1af1e38af7eeeefb2ba0a8940533311
Mel <garrillofeeling@gmail.com>
2025-01-03 12:41:36 -0400

    Implement mathematical operation evaluation

--- scripts/index.js
+++ scripts/index.js
@@ -2,17 +2,41 @@ let numbers;
 let inputField;
 let delButton;
 let ansButton;
+let equalButton;
+
+const operators = ["+", "-", "×", "÷"];
+
+const operatorPriority = {
+    "": -1,
+    "+": 2,
+    "-": 2,
+    "×": 1,
+    "÷": 0,
+};
+
+function Operator(type, index) {
+    this.type = type;
+    this.index = index;
+};
+
+function TreeNode(operator, left, right) {
+    this.operator = operator;
+    this.left = left;
+    this.right = right;
+};
 
 function loadData() {
     numbers = document.getElementById("numerical");
     inputField = document.getElementById("input");
     delButton = document.getElementById("delete");
     ansButton = document.getElementById("clear");
+    equalButton = document.getElementById("equals");
 
     loadButtons();
 
     delButton.addEventListener("click", del);
     ansButton.addEventListener("click", clear);
+    equalButton.addEventListener("click", processOperation);
 }
 
 function createNumberButton(number) {
@@ -40,6 +64,42 @@ function prepareOperationButtons() {
 const input = event => inputField.textContent += event.target.value;
 const del = () => inputField.textContent = (inputField.textContent.length > 0) ? inputField.textContent.slice(0, inputField.textContent.length - 1) : inputField.textContent;
 const clear = () => inputField.textContent = "";
+
+function processOperation() {
+    let operation = inputField.textContent;
+    console.log(operation);
+    // Create sintax tree
+    let tree = sintaxAnalysis(operation);
+    // Evaluate sintax tree from bottom to top
+}
+
+function sintaxAnalysis(operation, currentOperator = new Operator("", 0), currentIndex = 0) {
+    if (operators.includes(operation[currentIndex])) {
+        if (operatorPriority[operation[currentIndex]] == 2) {
+            return new TreeNode(
+                operation[currentIndex], 
+                sintaxAnalysis(operation.slice(0, currentIndex)), 
+                sintaxAnalysis(operation.slice(currentIndex + 1))
+            );
+        }
+        if (operatorPriority[currentOperator.type] < operatorPriority[operation[currentIndex]])
+            currentOperator = new Operator(operation[currentIndex], currentIndex);
+    }
+    if (currentIndex == operation.length - 1) {
+        if (currentOperator.type == "") {
+            return operation;
+        }
+        else {
+            return new TreeNode(
+                currentOperator.type, 
+                sintaxAnalysis(operation.slice(0, currentOperator.index)), 
+                sintaxAnalysis(operation.slice(currentOperator.index + 1))
+            );
+        }
+    }
+
+    return sintaxAnalysis(operation, currentOperator, currentIndex + 1);
+}
 
 function operate(operation, a, b) {
     switch (operation) {
